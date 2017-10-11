a87c34f2ae726146bd00902c3f17cf74e1655e17
Tomislav Eric <tomislav.eric@arconsis.com>
2017-10-11 11:19:09 +0200

    add INavigationService Interface with simple NavigateTo mthod

---
+++ Xamarin.Forms-MVVM-Template/Services/INavigationService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Xamarin.FormsMVVMTemplate.ViewModels;
+
+namespace Xamarin.FormsMVVMTemplate.Services
+{
+    public interface INavigationService
+    {
+        Task NavigateToAsync<TViewModel>() where TViewModel : ViewModelBase;
+    }
+}
