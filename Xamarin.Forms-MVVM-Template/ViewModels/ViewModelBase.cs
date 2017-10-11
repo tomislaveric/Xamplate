e3d5107f1205ea463735f183bf0d2db6c7297132
Tomislav Eric <tomislav.eric@arconsis.com>
2017-10-11 11:17:57 +0200

    add a base view model with propertychanged behaviour

---
+++ Xamarin.Forms-MVVM-Template/ViewModels/ViewModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Xamarin.FormsMVVMTemplate.ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyname = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+        }
+    }
+}
