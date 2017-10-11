84150b899d650306e393cec99281c365ea021790
Tomislav Eric <tomislav.eric@arconsis.com>
2017-10-11 11:10:33 +0200

    add NavigationPage title

--- Xamarin.Forms-MVVM-Template/ViewModels/StartViewModel.cs
+++ Xamarin.Forms-MVVM-Template/ViewModels/StartViewModel.cs
@@ -4,6 +4,7 @@ namespace Xamarin.FormsMVVMTemplate.ViewModels
     public class StartViewModel
     {
         public string WelcomeText => "Hello MVVM World";
+        public string PageTitle => "Start Page";
 
         public StartViewModel()
         {
