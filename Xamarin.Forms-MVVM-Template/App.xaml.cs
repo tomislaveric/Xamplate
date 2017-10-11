9275fabb874e043ffef5c4f7e5532aca619f553b
Tomislav Eric <tomislav.eric@arconsis.com>
2017-10-11 10:26:20 +0200

    create a blank xamarin forms app

---
+++ Xamarin.Forms-MVVM-Template/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace Xamarin.FormsMVVMTemplate
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Xamarin_Forms_MVVM_TemplatePage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Xamarin.Forms-MVVM-Template/Xamarin.Forms-MVVM-TemplatePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Xamarin.FormsMVVMTemplate
+{
+    public partial class Xamarin_Forms_MVVM_TemplatePage : ContentPage
+    {
+        public Xamarin_Forms_MVVM_TemplatePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
